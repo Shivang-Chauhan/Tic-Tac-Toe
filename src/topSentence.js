d0b53569d59eeac6bba18f84c32d5d44a4df8f2d
Shivang Chauhan <shivangchauhan@Shivangs-MacBook-Air.local>
2023-07-29 16:51:01 +0530

    firstCommit

---
+++ src/topSentence.js
@@ -0,0 +1,12 @@
+// TopSentence.js
+import React from 'react';
+
+const TopSentence = () => {
+  return (
+    <div className="top-sentence">
+      This is a sentence at the top of the webpage.
+    </div>
+  );
+};
+
+export default TopSentence;
